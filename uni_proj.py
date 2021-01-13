eb5631d04fe5782215aec841033616dceda39e39
saleh056 <saleh056ebrahimi@gmail.com>
2021-01-13 12:32:51 +0330

    Write the base code

---
+++ uni_proj.py
@@ -0,0 +1,99 @@
+class student:
+    
+    def first_name(self,item):
+        self.firstName = item
+
+    def last_name(self,item):
+        self.lastName = item
+
+    def national_code(self,item):
+        self.nationalCode = item
+
+    def student_code(self,item):
+        self.studentCode = item
+    
+    def father_name(self,item):
+        self.fatherName = item
+
+    def address(self,item):
+        self.Address = item
+
+    def phone_number(self,item):
+        self.phoneNumber = item
+
+    def user_name(self,item):
+        self.userName = item
+    
+    def password(self,item):
+        self.Password = item
+
+
+class master:
+
+    def first_name(self,item):
+        self.firstName = item
+
+    def last_name(self,item):
+        self.lastName = item
+
+    def national_code(self,item):
+        self.nationalCode = item
+
+    def father_name(self,item):
+        self.fatherName = item
+
+    def address(self,item):
+        self.Address = item
+
+    def phone_number(self,item):
+        self.phoneNumber = item
+    
+    def instructor_number(self,item):
+        self.instructorNumber = item
+    
+    def user_name(self,item):
+        self.userName = item
+    
+    def password(self,item):
+        self.Password = item
+
+
+class lesson:
+
+    def lesson_name(self,item):
+        self.lessonName = item
+    
+    def lesson_number(self,item):
+        self.lessonNumber = item
+
+
+class education_expert:
+
+    def first_name(self,item):
+        self.firstName = item
+
+    def last_name(self,item):
+        self.lastName = item
+
+    def national_code(self,item):
+        self.nationalCode = item
+
+    def user_name(self,item):
+        self.userName = item
+    
+    def password(self,item):
+        self.Password = item
+    
+    def father_name(self,item):
+        self.fatherName = item
+
+    def address(self,item):
+        self.Address = item
+
+    def phone_number(self,item):
+        self.phoneNumber = item
+
+    def employee_code(self,item):
+        self.employeeCode = item
+
+    
